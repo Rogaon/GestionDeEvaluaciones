5373ee0d9f1abdaee6eb0ae0e720ea5a2eef494d
Rogcito <samuelalvarez2000@gmail.com>
2024-08-28 03:43:47 -0400

    commit

--- src/gestiondeevaluaciones/Evaluacion.java
+++ src/gestiondeevaluaciones/Evaluacion.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gestiondeevaluaciones;
 
 import java.util.ArrayList;
@@ -10,10 +5,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 
-/**
- *
- * @author samue
- */
+
 public class Evaluacion {
     private String titulo;
     private List<Pregunta> preguntas;
